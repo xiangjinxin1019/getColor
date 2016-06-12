d7c6df9f78d7f27efe51cc317e848ea7655f3432
项金鑫 <jinxin_xiang@intsig-pc0294.local>
2016-06-12 16:30:28 +0800

    Initial Commit

---
+++ getColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  getColor
+//
+//  Created by 项金鑫 on 16/6/12.
+//  Copyright © 2016年 JX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ getColor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  getColor
+//
+//  Created by 项金鑫 on 16/6/12.
+//  Copyright © 2016年 JX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
